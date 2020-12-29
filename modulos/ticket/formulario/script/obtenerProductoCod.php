bece21597ddb06a2d4dd9f703759425c3ab3da24
diego gallardo <diegogallardo@eximben.cl>
2020-12-29 10:47:40 -0300

    se agrega validacion de caracteres extraños

--- modulos/ticket/formulario/script/obtenerProductoCod.php
+++ modulos/ticket/formulario/script/obtenerProductoCod.php
@@ -9,7 +9,7 @@
 		exit( "Error en la consulta SQL" );
 	}
 	while($resultado = odbc_fetch_array($rs)){ 
-		$descripcion = $resultado['DESC2']." ".$resultado['DESC3'];
+		$descripcion = utf8_encode($resultado['DESC2'])." ".utf8_encode($resultado['DESC3']);
 		$precio = number_format($resultado['PRICE01'],0,'','');
 		$upc = $resultado['UPC'];
 		$alu = $resultado['ALU'];
@@ -17,11 +17,67 @@
 	}
 	
 	$respuesta = array(
-		'descripcion'=>$descripcion,
+		'descripcion'=>sanear_string($descripcion),
 		'precio'=>$precio,
 		'upc'=>$alu,
 		'articulo'=>$articulo
 	);
 	echo json_encode($respuesta);
 	odbc_close( $conn );
+
+	function sanear_string($string) {
+		$string = trim($string);
+	
+		$string = str_replace(
+			array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+			array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+			$string
+		);
+	
+		$string = str_replace(
+			array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+			array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+			$string
+		);
+	
+		$string = str_replace(
+			array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+			array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+			$string
+		);
+	
+		$string = str_replace(
+			array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+			array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+			$string
+		);
+	
+		$string = str_replace(
+			array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+			array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+			$string
+		);
+	
+		$string = str_replace(
+			array('ñ', 'Ñ', 'ç', 'Ç'),
+			array('n', 'N', 'c', 'C',),
+			$string
+		);
+	
+		//Esta parte se encarga de eliminar cualquier caracter extraño
+		$string = str_replace(
+			array("\\", "¨", "º", "-", "~",
+				 "#", "@", "|", "!", "\"",
+				 "·", "$", "%", "&", "/",
+				 "(", ")", "?", "'", "¡",
+				 "¿", "[", "^", "`", "]",
+				 "+", "}", "{", "¨", "´",
+				 ">", "< ", ";", ",", ":",
+				 ".", ""),
+			'',
+			$string
+		);
+	
+		return $string;
+	}
 ?>
